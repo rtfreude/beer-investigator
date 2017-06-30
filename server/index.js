9c0983a1eac05d9fed1f4a0bf48dacf387d59aae
Ryan Freude <freudehack@gmail.com>
2017-06-30 14:18:53 -0500

    updated port

--- server/index.js
+++ server/index.js
@@ -4,7 +4,10 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 3003;
 
-app.listen(process.env.PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-});
+// app.listen(PORT, () => {
+//   console.log(`App listening on port ${PORT}!`);
+// });
 
+app.listen(PORT, '0.0.0.0', function(err) {
+  console.log("Started listening on %s", app.url);
+});
